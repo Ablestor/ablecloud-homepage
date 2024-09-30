df0ed2f96f331d4a575d91a6eacd188e757a1df7
Kim jonghun <developerkimjh@gmail.com>
2024-09-30 15:18:58 +0900

    FEAT: add ebook

--- src/pages/ebook/index.tsx
+++ src/pages/ebook/index.tsx
@@ -8,6 +8,7 @@ import Logo_Article_1_1 from '@/public/images/blog/ebook_1.png';
 import Logo_Article_1_2 from '@/public/images/blog/ebook_2.png';
 import Logo_Article_1_3 from '@/public/images/blog/ebook_3.png';
 import Logo_Article_1_4 from '@/public/images/blog/ebook_4.png';
+import Logo_Article_1_5 from '@/public/images/blog/ebook_5.png';
 
 export default function VideoPage() {
   return (
@@ -89,6 +90,22 @@ export default function VideoPage() {
               <div className='text-[16px] font-bold'>ABLESTACK VMWare 가상머신 마이그레이션</div>
               <div className='text-[14px]'>내장된 마이그레이션 도구를 통한 V2V 가상머신 이관</div>
               <Link href='/pdf/ABLESTACK_VMWare.pdf'>
+                <a
+                  className='flex h-[47px] cursor-pointer items-center gap-[10px] text-[14px] font-bold text-[#0085F2]'
+                  target='_blank'>
+                  백서 바로보기
+                  <Arrow_Icon />
+                </a>
+              </Link>
+            </div>
+          </div>
+          <div className='border border-[#D4D4D4]'>
+            <Image src={Logo_Article_1_5} />
+            <div className='flex flex-col gap-[10px] p-[16px]'>
+              <div className='text-[12px] text-[#919191]'>ABLESTACK</div>
+              <div className='text-[16px] font-bold'>ABLESTACK 물리서버를 가상머신으로</div>
+              <div className='text-[14px]'>물리서버를 가상머신으로 마이그레이션하는 방법</div>
+              <Link href='/pdf/ABLESTACK_VMWare_Server.pdf'>
                 <a
                   className='flex h-[47px] cursor-pointer items-center gap-[10px] text-[14px] font-bold text-[#0085F2]'
                   target='_blank'>
